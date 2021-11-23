10d8968a679b3926c94155645d05e1975468e9c6
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2021-11-24 00:29:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ class HackerModeInstaller:
             if os.path.exists(hackermode_command_line_path):
                 os.remove(hackermode_command_line_path)
             os.system(
-                f'curl https://raw.githubusercontent.com/Arab-developers/PSHMode/main/install.sh > PSHMode.install && bash PSHMode.install')
+                f'curl https://raw.githubusercontent.com/Arab-developers/PSHMode/main/install.sh > PSHMode.install 2> .PSHMode-install.log && bash PSHMode.install')
         else:
             print("# can't update in the DEUBG mode!")
 
